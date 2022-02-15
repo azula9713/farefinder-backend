3dae6d99913cccab689b7f98d1b00d0ae626fe6c
azula9173 <nnviduranga@gmail.com>
2022-02-15 18:23:09 +0530

    Added User and Session

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,21 @@
+const dbUsername = 'ffadmin';
+const dbPassword = '6GoW8gacrsWqtbU1';
+
 export default {
   port: 1337,
-  dbUri: 'mongodb://localhost:27017/test',
+  redis: {
+    port: 6379,
+    host: 'localhost',
+    password: 'secret',
+  },
+  session: {
+    secret: 'secretkey',
+    resave: false,
+    name: 'sid',
+    idleTimeout: 1000 * 60 * 30,
+  },
+  dbUri: `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.6dsw4.mongodb.net/farefinder?retryWrites=true&w=majority`,
+  // dbUri: `mongodb+srv://${dbUsername}:${dbPassword}@localhost:27017/farefinder`,
   token: '85de6f3a6caba8d221a1106573762b5f',
   marker: 189998, //affiliateID
   currency: 'usd',
--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,52 @@ dotenv.config();
 import express from 'express';
 import config from 'config';
 import cors from 'cors';
+import Redis, { RedisOptions } from 'ioredis';
+import session, { SessionOptions } from 'express-session';
+import connectRedis from 'connect-redis';
 
 import logger from './utils/logger';
 import routes from './routes/routes.index';
+import dbConnect from './utils/dbConnect';
 
 const port = config.get<number>('port');
+
+const REDIS_OPTIONS: RedisOptions = {
+  host: config.get<string>('redis.host'),
+  port: config.get<number>('redis.port'),
+  password: config.get<string>('redis.password'),
+};
+
+const SESSION_OPTIONS: SessionOptions = {
+  secret: config.get<string>('session.secret'),
+  name: config.get<string>('session.name'),
+  resave: config.get<boolean>('session.resave'),
+  cookie: {
+    maxAge: config.get<number>('session.idleTimeout'),
+    //secure if application is on PROD
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: true,
+  },
+  rolling: true,
+  saveUninitialized: false,
+};
+
+const RedisStore = connectRedis(session);
+const redisClient = new Redis(REDIS_OPTIONS);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  session({
+    ...SESSION_OPTIONS,
+    store: new RedisStore({ client: redisClient }),
+  }),
+);
 
 app.listen(port, async () => {
   logger.info(`App is listening on port at localhost:${port}`);
-  // await connect();
+  await dbConnect();
   routes(app);
 });
