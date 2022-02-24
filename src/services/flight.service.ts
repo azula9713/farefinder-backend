6228ce7912a841dd68b8326f8c2bf91cbeabfbdc
azula9173 <nnviduranga@gmail.com>
2022-02-25 00:17:39 +0530

    Minor

--- src/services/flight.service.ts
+++ src/services/flight.service.ts
@@ -39,8 +39,6 @@ export const fetchSearchResults = async (searchId: string) => {
     } else {
       return resp;
     }
-
-    // return resp;
   } catch (error) {
     logger.error(error);
   }
@@ -48,29 +46,12 @@ export const fetchSearchResults = async (searchId: string) => {
 
 export const restructreSearchResults = (data: any) => {
   const flights: any[] = [];
-  let allDifferent = false;
 
   try {
     data.forEach((agent: any) => {
       if (agent?.proposals) {
         agent.proposals.forEach((proposal: any) => {
-          proposal?.segment?.forEach((segment: any, segIn: number) => {
-            segment.flight.forEach((flight: any, flightIn: number) => {
-              if (
-                flights.find(
-                  (x) =>
-                    x.segment[segIn]?.flight[flightIn]?.number === flight.number &&
-                    x.segment[segIn]?.flight[flightIn]?.local_arrival_timestamp === flight.local_arrival_timestamp,
-                ) === undefined
-              ) {
-                allDifferent = true;
-              } else {
-                allDifferent = false;
-              }
-            });
-          });
-
-          if (allDifferent && flights.find((x) => x.sign === proposal.sign) === undefined) {
+          if (flights.find((f) => f.sign === proposal.sign) === undefined) {
             flights.push(proposal);
           }
         });
