166f2a268196c1803d6f2b4a05af5b048c7c5ebe
azula9173 <nnviduranga@gmail.com>
2022-03-08 11:43:17 +0530

    Changed to local URI

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -9,8 +9,9 @@ const dbConnect = async () => {
   const dbHost = config.get<string>('mongo.host');
 
   const dbUri = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
+  const localUri = `mongodb://${dbHost}/${dbName}`;
   try {
-    await mongoose.connect(dbUri);
+    await mongoose.connect(localUri);
     logger.info('Connected to MongoDB');
   } catch (err) {
     logger.error('Error connecting to MongoDB', err);
