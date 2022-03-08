2d4f81a5448d2635075c4b040413f7ff534aa924
azula9173 <nnviduranga@gmail.com>
2022-03-08 21:34:39 +0530

    Hotel Script Fix

--- src/routes/hotelScript.routes.ts
+++ src/routes/hotelScript.routes.ts
@@ -10,18 +10,18 @@ import {
   getHotelScriptHandler,
 } from '../controller/hotelScript.controller';
 import {
-  createPopularLocationSchema,
-  deletePopularLocationSchema,
-  updatePopularLocationSchema,
-  getPopularLocationSchema,
-} from '../schemas/popularLocation.schema';
+  createHotelScriptSchema,
+  deleteHotelScriptSchema,
+  updateHotelScriptSchema,
+  getHotelScriptSchema,
+} from '../schemas/hotelScript.schema';
 
 const router = Router();
 
 router.route('/all').get(getAllHotelScriptsHandler);
-router.route('/view/:hotelScriptId').get(validate(getPopularLocationSchema), getHotelScriptHandler);
-router.route('/create').post([requireUser, validate(createPopularLocationSchema)], createHotelScriptHandler);
-router.route('/update/:hotelScriptId').put([requireUser, validate(updatePopularLocationSchema)], updateHotelScriptsHandler);
-router.route('/delete/:hotelScriptId').delete([requireUser, validate(deletePopularLocationSchema)], deleteHotelScriptsHandler);
+router.route('/view/:hotelScriptId').get(validate(getHotelScriptSchema), getHotelScriptHandler);
+router.route('/create').post([requireUser, validate(createHotelScriptSchema)], createHotelScriptHandler);
+router.route('/update/:hotelScriptId').put([requireUser, validate(updateHotelScriptSchema)], updateHotelScriptsHandler);
+router.route('/delete/:hotelScriptId').delete([requireUser, validate(deleteHotelScriptSchema)], deleteHotelScriptsHandler);
 
 export default router;
